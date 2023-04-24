412bd0c546587118d6b40e7adb0b796c2187db5d
Marlon <marlonfg91@gmail.com>
2023-04-24 12:08:58 -0400

    Not run seed locale

--- updates/seed_locale.php
+++ updates/seed_locale.php
@@ -1,19 +1,19 @@
 <?php namespace MarlonFreire\Sitios\Updates;
 
 use October\Rain\Database\Updates\Seeder;
-use RainLab\Translate\Models\Locale;
+//use RainLab\Translate\Models\Locale;
 
 class SeedLocale extends Seeder
 {
 
     public function run()
     {
-        Locale::create([
+        /* Locale::create([
             'code' => 'es',
             'name' => 'Español',
             'is_default' => true,
             'is_enabled' => true
-        ]);
+        ]); */
     }
 
 }
